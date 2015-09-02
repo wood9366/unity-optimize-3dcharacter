41a8b0295e7a1dc0e9a8ee4015c4b856df54b476
fpg666 <fpg666@hotmail.com>
2015-09-02 18:51:59 +0800

    * add camera preview controller

---
+++ Assets/Scripts/Util/CameraPreviewController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPreviewController : MonoBehaviour {
+	public Camera Cam;
+
+	public Vector3 Target = Vector3.zero;
+	public float Distance = 100;
+	public float AngleA = 45;
+	public float RotateSpeed = 10;
+
+	private float _angleB = 0;
+
+	public void play() {
+		_isPlay = true;
+	}
+
+	public void stop() {
+		_isPlay = false;
+	}
+
+	public float AngleB {
+		get { return _angleB * Mathf.Rad2Deg; }
+		set {
+			_angleB = value * Mathf.Deg2Rad;
+			updateCam();
+		}
+	}
+
+	void Start () {
+		_angleB = 0;
+	}
+	
+	void Update () {
+		if (_isPlay) {
+			_angleB += RotateSpeed * Time.deltaTime;
+
+			if (_angleB > 360) {
+				_angleB -= 360;
+			}
+
+			updateCam();
+		}
+	}
+
+	void updateCam() {
+		float sina = Mathf.Sin(AngleA * Mathf.Deg2Rad);
+		float cosa = Mathf.Cos(AngleA * Mathf.Deg2Rad);
+		float sinb = Mathf.Sin(_angleB);
+		float cosb = Mathf.Cos(_angleB);
+		
+		Cam.transform.position = new Vector3(Distance * cosa * cosb,
+		                                     Distance * sina,
+		                                     Distance * cosa * sinb);
+		
+		Quaternion rot = Quaternion.identity;
+		rot.SetLookRotation(Target - Cam.transform.position);
+		
+		Cam.transform.rotation = rot;
+	}
+
+	bool _isPlay = false;
+}
---
+++ Assets/Scripts/Util/Editor/CameraPreviewControllerInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CameraPreviewController))]
+public class CameraPreviewControllerInspector : Editor {
+	void OnEnable() {
+		_preview = target as CameraPreviewController;
+	}
+
+	public override void OnInspectorGUI() {
+		GUILayout.BeginVertical();
+
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("play")) _preview.play();
+		if (GUILayout.Button("stop")) _preview.stop();
+		GUILayout.EndHorizontal();
+
+		_preview.AngleB = GUILayout.HorizontalSlider(_preview.AngleB, 0, 359);
+
+		GUILayout.EndVertical();
+
+		DrawDefaultInspector();
+	}
+
+	private CameraPreviewController _preview = null;
+}
